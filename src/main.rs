bb275d9b18573936f8f94d456676b92c6fe4b8af
Enrique Tebari Barragan Corte <tebari@tebari.com>
2020-02-18 21:40:28 -0800

    cli arguments for image resolution

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ fn main() {
     println!("Trace all the rays!");
     let args: Vec<String> = env::args().collect();
     let renderer = &args[1];
+    let width_str = args.get(2);
+    let height_str = args.get(3);
 
-    let width = 1600;
-    let height = 800;
+    let width = get_uint_or(width_str, 200);
+    let height = get_uint_or(height_str, 100);
 
     let image = match renderer.as_ref() {
         "gradient" => renderers::draw_gradient(width, height),
@@ -25,6 +27,12 @@ fn main() {
     println!("Write file");
     write_to_file("output/render.ppm", &ppm_str).unwrap();
     println!("Rays have been traced!");
+}
+
+fn get_uint_or(number: Option<&String>, default: u32) -> u32 {
+    number.map(
+        |num_str| num_str.parse::<u32>().unwrap_or(default)
+    ).unwrap_or(default)
 }
 
 fn write_to_file(filepath: &str, data: &String) -> Result<(),io::Error> {
